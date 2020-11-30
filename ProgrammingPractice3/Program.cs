86bfe829fe4676d26b93f93200648020e96d3f50
Cormac Mc Manus <S00197425@mail.itsligo.ie>
2020-11-30 10:58:23 +0000

    First Commit

---
+++ ProgrammingPractice3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProgrammingPractice3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ProgrammingPractice3/Song.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingPractice3
+{
+    class Song
+    {
+
+    }
+}
