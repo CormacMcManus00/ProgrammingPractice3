c43e46b1894d291f523005907f9b34fb5ebaa435
Cormac Mc Manus <S00197425@mail.itsligo.ie>
2020-11-30 12:04:04 +0000

    Final

--- ProgrammingPractice3/Program.cs
+++ ProgrammingPractice3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ProgrammingPractice3
 {
@@ -6,7 +7,53 @@ namespace ProgrammingPractice3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Song s1 = new Song("BEAUTIFUL PEOPLE", "ED SHEERAN", 3.15, Genre.Pop);
+            Song s2 = new Song("DANCE MONKEY", "TONES & I", 4.20, Genre.Dance);
+            Song s3 = new Song("CIRCLES", "POST MALONE", 3.25, Genre.Pop);
+            Song s4 = new Song("RIDE IT", "REGARD", 3.37, Genre.Dance);
+            Song s5 = new Song("SOUTH OF THE BORDER", "ED SHEERAN", 4.26, Genre.Pop);
+
+            List<Song> playlist = new List<Song>();
+            playlist.Add(s1);
+            playlist.Add(s2);
+            playlist.Add(s3);
+            playlist.Add(s4);
+            playlist.Add(s5);
+
+            Display(playlist);
+
+            playlist.Sort();
+
+            Display(playlist);
+
+            Shuffle(playlist);
+
+            Display(playlist);
+        }
+
+        private static void Display(List<Song> playlist)
+        {
+            Console.WriteLine("\n{0, -20}{1, -25}{2, -10}{3, -10}", "Artist","Song","Duration","Genre");
+
+            foreach (Song song in playlist)
+            {
+                Console.WriteLine($"{song.Artist, -20}{song.Title, -25}{song.Duration, -10}{song.MusicGenre, -10}");
+            }
+        }
+
+        private static void Shuffle(List<Song> playlist)
+        {
+            Random rng = new Random();
+            int numberOfSongs = playlist.Count;
+
+            while (numberOfSongs > 1)
+            {
+                numberOfSongs--;
+                int randomNumber = rng.Next(numberOfSongs + 1);
+                Song song = playlist[randomNumber];
+                playlist[randomNumber] = playlist[numberOfSongs];
+                playlist[numberOfSongs] = song;
+            }
         }
     }
 }
--- ProgrammingPractice3/Song.cs
+++ ProgrammingPractice3/Song.cs
@@ -1,11 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Drawing;
 using System.Text;
 
 namespace ProgrammingPractice3
 {
-    class Song
+    public class Song:IComparable
     {
+        //properties
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public double Duration { get; set; }
+        public Genre MusicGenre { get; set; }
 
+        //constructors
+        public Song(string title, string artist, double duration, Genre musicGenre)
+        {
+            Title = title;
+            Artist = artist;
+            Duration = duration;
+            MusicGenre = musicGenre;
+
+        }
+
+        public Song(string title, string artist):this(title,artist,0,Genre.Other)
+        {
+            Title = title;
+            Artist = artist;
+
+            Duration = 0;
+            MusicGenre = Genre.Other;
+
+
+        }
+
+        public Song():this("unknown", "unknown") { }
+
+
+
+        //methods
+
+        public override string ToString()
+        {
+            return string.Format($"{Title} {Artist} {Duration} {MusicGenre}");
+        }
+
+        public int CompareTo(object other)
+        {
+            Song that = (Song)other;
+
+            int returnvalue = this.Artist.CompareTo(that.Artist); //Sort by artist
+
+            if (returnvalue == 0)
+            {
+                returnvalue = this.Title.CompareTo(that.Title); //Sort by title
+            }
+
+            return returnvalue;
+        }
     }
+
+    public enum Genre { Rock, Pop, Dance, Other}
 }
